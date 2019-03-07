e7b5d34f85f26dd5d4c85c87db9a9f121504c855
taravell <41087349+taravell@users.noreply.github.com>
2019-03-07 20:34:17 -0300

    Add files via upload
    Framework feito em python 3

---
+++ Framework.py
@@ -0,0 +1,22 @@
+#Apology Framework v.01
+
+#!/usr/bin/python3
+# - * - codificação utf-8 - * -
+
+#blibliotecas
+import sys
+import os
+
+def main():
+	try:
+		print("Apology Framework")
+		def inicio1():
+			while True:
+				print("[1] instalar [2]sair")
+				repo = input("selecione as opções")    
+                                while opc == "1":
+                                        print("[1]instalar [2]sair ")
+				if repo == "1":
+					cmd1 = os.system("apt-get install nmap")
+				elif repo  "2":
+					cmd1 = os.system("exit")
