89de29141337f109974adadae9105e4e08f28efa
taravell <41087349+taravell@users.noreply.github.com>
2019-03-07 20:46:17 -0300

    Update Framework.py

--- Framework.py
+++ Framework.py
@@ -14,7 +14,7 @@ def main():
 			while True:
 				print("[1] instalar [2]sair")
 				repo = input("selecione as opções")    
-                                while opc == "1":
+                			while opc == "1":
                                         print("[1]instalar [2]sair ")
 				if repo == "1":
 					cmd1 = os.system("apt-get install nmap")
